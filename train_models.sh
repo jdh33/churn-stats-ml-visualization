36face15d8c4f3166a0b6e1a2953d87bee429fe3
Jonah Hocum <jonahdhocum@gmail.com>
2024-01-04 11:51:32 -0800

    Update train_models.sh Update to coincide with changes in train_classification_models.py

--- train_models.sh
+++ train_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+VARIABLES_TO_DROP="MonthlyCharges,TotalCharges,Tenure"
 N_JOBS=1
 
-python scripts/train_models.py --training_dataset "telco-customer-churn/processed/" \
-        --target_variable "Churn" --train_with_shuffled_data \
-        --timestamp --n_jobs=$N_JOBS
+python src/models/train_classification_models.py --training_dataset "telco-customer-churn" \
+        --target_variable "Churn" --variables_to_drop $VARIABLES_TO_DROP \
+        --n_jobs=$N_JOBS
